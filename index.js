cae57a203fdeb32e9a8be0de88812333e03b5a4a
ShivaPrime82 <jeremy.clair.82@gmail.com>
2020-03-25 15:01:29 +0100

    config mysql

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const express = require("express");
+const mysql = require("mysql");
+const settings = require("./settings.json");
+
+const sqlConfig = settings.sqlConfig;
 
 const app = express();
 
 app.listen(3000, () => {
     console.log("SERVER STARTED");  
+});
+
+app.get("/", (req, res) => {
+    const sqlConnection = mysql.createConnection(sqlConfig);
+ 
+    sqlConnection.query(
+        "SELECT id, email, firstname, lastname, birthdate FROM node_users",
+        (error, result) => {
+            if (error) {
+                console.log("ERROR", error.code);           
+            } else {
+                console.log("RESULT", result);      
+            }
+            sqlConnection.end();
+        }
+    );
+
+    res.send({
+        id        : 1,
+        email     : "jeremy.clair.82@gmail.com",
+        firstname : "Jérémy",
+        lastname  : "Clair",
+        birthdate : new Date(1992, 11, 4),
+    });
 });
